2fd382d5f4c68e68975953689227cf195cbffa17
Mateus Soares <55162817+mateus123soares@users.noreply.github.com>
2024-11-04 04:11:06 -0300

    teste

--- firewall/__tests__/unit/create_new_rule_opnsense.js
+++ firewall/__tests__/unit/create_new_rule_opnsense.js
@@ -1,6 +1,6 @@
 const { faker } = require('@faker-js/faker');
 const firewall = require('../../src/api/service/grafana/parseAlerts');
-
+const dropRulesIpService = require('../../src/api/service/firewall/FirewallDropService');
 
 // A rota deve criar uma nova regra no firewall OPNSense
 describe('A rota deve criar uma nova regra (IP) no Firewall OPNSense', () => {
@@ -20,6 +20,24 @@ describe('A rota deve criar uma nova regra (Port) no Firewall OPNSense', () => {
     const response = await firewall.parseAlerts({
       alertname: "Total TCP/UDP Port Destination Requests",
       ip: faker.internet.ipv4()
+    });
+
+    expect(response.code).toBe(200);
+  });
+});
+
+// A rota deve criar uma nova regra no firewall OPNSense
+describe('A rota deve criar uma nova regra no Firewall OPNSense', () => {
+  it('Deve bloquear um IP no OPNSense', async () => {
+    const response = await dropRulesIpService.dropRulesIp({
+      description: `Teste Automatizado`,
+      action: 'block',
+      direction: 'in',
+      source_net: `faker.internet.ipv4()`,
+      destination_net: 'any',
+      protocol: 'any',
+      interface: 'lan,wan',
+      log: '1',
     });
 
     expect(response.code).toBe(200);
